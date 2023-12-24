33442a91e53ea22531e77915eb177def1f92307a
Dev-Varshney <devvarshney7037@gmail.com>
2023-12-24 22:07:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+    bar.addEventListener('click' , ()=>{
+        document.getElementById('dev').innerHTML="Dev varshney";
+        nav.classList.add('active1');
+    })
+
+
+
