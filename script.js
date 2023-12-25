000c1e2f1aaa4b5e96542fbde797606a43ff5257
Dev-Varshney <devvarshney7037@gmail.com>
2023-12-25 16:56:20 +0530

    second committ

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-
 const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
 
-    bar.addEventListener('click' , ()=>{
-        document.getElementById('dev').innerHTML="Dev varshney";
-        nav.classList.add('active1');
-    })
+const nav = document.getElementById('navbar');
+const back = document.getElementById("back");
+
+bar.addEventListener('click', () => {
+    console.log("Clicked!");
+    nav.style.right = "0";
+    back.style.display = "block";
+})
+
+back.addEventListener('click', () => {
+    console.log("Back!");
+    nav.style.right = "-300px";
+    back.style.display = "none";
+})
 
 
 
